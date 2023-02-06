a6451750d44c1c1d9fed33b1aef7db3fc5c2a16e
Diego Nazareno <diegonazarenocaceres@gmail.com>
2023-02-06 20:31:07 -0300

    Prueba N 1 repositorio

---
+++ js/funciones.js
@@ -0,0 +1,7 @@
+document.getElementById("boton_formulario").onclick = function () {
+    document.getElementById("formulario").style.display = "block";
+
+} 
+document.getElementById("boton_up").onclick = function () {
+    document.documentElement.scrollTop = 0 ;
+}
